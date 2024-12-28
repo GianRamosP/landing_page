6c7fdf4450f3489dcdb5dc96485096bf95d2e0e2
GianRamosP <gian21022001@gmail.com>
2024-12-28 11:58:15 -0500

    ASISTE SAC

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -77,7 +77,7 @@ export default function Footer() {
                   <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z" />
                 </svg>
               </span>
-              LPage
+              ASISTE S.A.C.
             </h6>
             <p>
               Here you can use rows and columns to organize your footer content.
@@ -136,7 +136,7 @@ export default function Footer() {
                   <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
                 </svg>
               </span>
-              New York, NY 10012, US
+              Jr. Los Pinos Mz. 38 lote 12 Yarinacocha - Ucayali
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
@@ -149,7 +149,7 @@ export default function Footer() {
                   <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                 </svg>
               </span>
-              info@example.com
+              asiste.jfsac@fmail.com
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
@@ -165,9 +165,9 @@ export default function Footer() {
                   />
                 </svg>
               </span>
-              + 01 234 567 88
+              940047242 - 912277680 - 901065040
             </p>
-            <p className="flex items-center justify-center md:justify-start">
+            {/* <p className="flex items-center justify-center md:justify-start">
               <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -181,15 +181,15 @@ export default function Footer() {
                   />
                 </svg>
               </span>
-              + 01 234 567 89
-            </p>
+              940047242 - 912277680 - 901065040
+            </p> */}
           </div>
         </div>
       </div>
       <div className="bg-black/5 p-6 text-center">
-        <span>© 2024 Copyright: </span>
+        <span>© 2025 Copyright: </span>
         <a className="font-semibold" href="#">
-          LPage
+          ASISTE S.A.C.
         </a>
       </div>
     </footer>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
-          Contact Us
+          Contáctanos
         </h2>
         
         {/* Línea decorativa similar a la de "What We Do" */}
